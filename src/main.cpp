8e7b8536fbb2c7304fb1b824dcda320c0f06a65f
thep1ckaxe91 <ThePickaxe91@gmail.com>
2024-08-26 23:10:50 +0700

    done configure, now is just blasting

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,11 @@ int main(int argc, char **argv) {
     pos += Vec2(1,2);
     std::cout << pos.x << ' ' << pos.y << std::endl;
 
+    /*
+    Everything is linking correctly
+    The rest is up to you
+    TODO: go for it
+    */
+
     return 0;
 }
